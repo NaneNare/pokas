f51d59ca5d867eb76acecc63f17c507d0ef66647
Nane Hambardzumyan <nane_h@instigatemobile.com>
2016-10-17 01:17:02 +0400

    Added the initial skeleton of the project.

---
+++ Pokas/app/src/main/java/com/pokas/pokas/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pokas.pokas.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pokas.pokas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
